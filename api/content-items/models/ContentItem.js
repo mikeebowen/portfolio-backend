4bccae901f9325602efb92081a2c01a326c40513
Michael Bowen <mikee.bowen@gmail.com>
2017-07-24 23:12:51 -0700

    feat(ContentItem): add ContentItem model

---
+++ api/content-items/models/ContentItem.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const contentItemSchema = mongoose.Schema({
+  author: String,
+  content: String,
+  description: String,
+  image: {
+    src: String,
+    name: String
+  },
+  postType: String,
+  subtitle: String,
+  title: {
+    type: String,
+    required: true
+  },
+  uniqueTitleKey: {
+    type: String,
+    default: Date.now().toString()
+  }
+});
+
+contentItemSchema.virtual('uniqueTitle').get(function () {
+  return `${this.title.toLowerCase().toString().replace(/[^a-z0-9+]+/gi, '+')}-${this.uniqueTitleKey}`;
+});
+
+module.exports = mongoose.model('ContentItem', contentItemSchema);
---
+++ api/content-items/models/ContentItem.test.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+const Mockgoose = require('mockgoose').Mockgoose;
+const mockgoose = new Mockgoose(mongoose);
+const expect = require('chai').expect;
+
+const ContentItem = require('./ContentItem');
+
+describe('ContentItem Model', function() {
+  //extend timeout for mockgoose
+  this.timeout(120000);
+
+  before(function(done) {
+
+    mockgoose.prepareStorage().then(() => {
+      mongoose.connect('mongodb://example.com/TestingDB', (err) => {
+        if (err) done(err);
+        done();
+      });
+    });
+  });
+
+  it('should require title', function (done) {
+    const testContentItem = new ContentItem({
+      description: 'the quick brown fox jumped over the lazy brown dog'
+    });
+
+    testContentItem.save((err) => {
+      expect(err).to.exist;
+      expect(err.message).to.equal('ContentItem validation failed: title: Path `title` is required.');
+      done();
+    });
+  });
+
+  it('should create a ContentItem when title is included title', function (done) {
+    const testContentItem = new ContentItem({
+      title: 'Gulliver\'s Travels'
+    });
+
+    testContentItem.save((err, item) => {
+      expect(err).to.not.exist;
+      expect(item.title).to.equal(testContentItem.title);
+      done();
+    });
+  });
+
+  it('should create a ContentItem and set the uniqueTitle', function (done) {
+    const testContentItem = new ContentItem({
+      title: 'Gulliver\'s Travels'
+    });
+
+    testContentItem.save((err, item) => {
+      expect(err).to.not.exist;
+      expect(item.uniqueTitle).to.contain('gulliver+s+travels-');
+      expect(item.uniqueTitle.length).to.equal(32);
+      done();
+    });
+  });
+
+  // restoring everything back
+  after( (done) => {
+
+    mockgoose.prepareStorage()
+      .then(() => {
+        mockgoose.helper.reset().then(() => {
+          mongoose.connection.close((err) => {
+            if (err) done(err);
+            done();
+          });
+        });
+      });
+  });
+});
