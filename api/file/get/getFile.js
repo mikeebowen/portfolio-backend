acbe4f4b7123fc891ba3578256c275529dd44d45
Michael Bowen <mikee.bowen@gmail.com>
2017-08-06 11:58:18 -0700

    style(getFile): linting

--- api/file/get/getFile.js
+++ api/file/get/getFile.js
@@ -12,7 +12,8 @@ const pathToFilesFolder = path.join(__dirname, '..', '..', '..', 'uploads');
 /**
  * method to serve image
  * @function serveImage
- * @param {Object} req - express request object, must contain params object with the key fileName that contains the name of the file found as a string
+ * @param {Object} req - express request object,
+ * must contain params object with the key fileName that contains the name of the file found as a string
  * @param {Object} res - express response object
  * @param {Function} next - express callback
  */
@@ -20,7 +21,7 @@ function serveImage(req, res, next) {
   if (!req.params.fileName) {
     next();
   } else {
-
+    
     fs.readdir(pathToFilesFolder, (err, files) => {
       /* istanbul ignore if */
       if (err) {
--- api/file/get/getFile.test.js
+++ api/file/get/getFile.test.js
@@ -4,78 +4,80 @@ const expect = require('chai').expect;
 const sinon = require('sinon');
 const proxyquire = require('proxyquire');
 
-const getFile = require('./getFile');
-
 describe('getFile', function () {
-
+  
   it('should return an image file if one is found', function (done) {
     const fsStub = {
       readdir: function (path, callback) {
         callback(null, ['another-fake-image.jpg', 'test-image.jpg']);
       }
     };
-    let getFile = proxyquire('./getFile', { 'fs': fsStub });
+    // eslint-disable-next-line prefer-const
+    let getFile = proxyquire('./getFile', {'fs': fsStub});
     const req = {
       params: {
         fileName: 'test-image.jpg'
       }
     };
-
+    
     getFile(req, {}, (err) => {
       if (err) done(err);
       expect(err).to.not.exist;
-      expect(req.fileName)
-        .to
-        .equal('test-image.jpg');
+      expect(req.fileName).to.equal('test-image.jpg');
+      
       done();
     });
   });
-
+  
   it('should not set req.fileName when no image is found', function (done) {
     const fsStub = {
       readdir: function (path, callback) {
         callback(null, ['another-fake-image.jpg', 'test-image.jpg']);
       }
     };
-    let getFile = proxyquire('./getFile', { 'fs': fsStub });
+    // eslint-disable-next-line prefer-const
+    let getFile = proxyquire('./getFile', {'fs': fsStub});
     const req = {
       params: {
         fileName: 'test-image-not-in-directory.jpg'
       }
     };
     const res = {};
-
+    
     getFile(req, res, (err) => {
       if (err) done(err);
       expect(err).to.not.exist;
       expect(req.fileName).to.be.undefined;
+      
       done();
     });
   });
-
+  
   it('should not set req.fileName when no params are included', function (done) {
     const fsStub = {
       readdir: function (path, callback) {
         callback(null, ['another-fake-image.jpg', 'test-image.jpg']);
       }
     };
-    let getFile = proxyquire('./getFile', { 'fs': fsStub });
+    // eslint-disable-next-line prefer-const
+    let getFile = proxyquire('./getFile', {'fs': fsStub});
     const req = {
       params: {
         // fileName: 'test-image.jpg'
       }
     };
     const res = {};
-
+    
     getFile(req, res, (err) => {
       if (err) done(err);
       expect(req.fileName).to.be.undefined;
+      
       done();
     });
   });
-
+  
   it('should call the errorHandler when an error is returned', function (done) {
-
+    
     const req = {
       params: {
         fileName: 'tuna.js'
@@ -91,23 +93,22 @@ describe('getFile', function () {
         return '/fake/path';
       }
     };
-
+    
     function fileTypeSub() {
-      return { ext: '.png' };
+      return {ext: '.png'};
     }
-
-    // tslint:disable-next-line:prefer-const
+    
+    // eslint-disable-next-line prefer-const
     let getFile = proxyquire('./getFile', {
       'file-type': fileTypeSub,
       'local-express-error-handler': expressErrorHandlerSpy,
       'path': pathStub
     });
-
+    
     getFile(req, {});
     setTimeout(() => {
-      expect(expressErrorHandlerSpy.called)
-        .to
-        .equal(true);
+      expect(expressErrorHandlerSpy.called).to.equal(true);
+      
       done();
     }, 100);
   });
