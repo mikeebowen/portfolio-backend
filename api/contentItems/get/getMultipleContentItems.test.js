11f8ce39b71438ea7931763d09af89efd3bed6ae
Michael Bowen <mikee.bowen@gmail.com>
2017-09-17 12:28:23 -0700

    fix errors found when connecting to client
    items are now sorted by date if not otherwise specified and when a search query is specified only items that have a queryScore of at least one are returned

--- api/contentItems/get/getMultipleContentItems.test.js
+++ api/contentItems/get/getMultipleContentItems.test.js
@@ -6,7 +6,7 @@ mongoose.Promise = Promise;
 const Mockgoose = require('mockgoose').Mockgoose;
 const mockgoose = new Mockgoose(mongoose);
 const sinon = require('sinon');
-const proxyquire = require('proxyquire');
+require('sinon-mongoose');
 
 const ContentItem = require('../models/ContentItem');
 const getMultipleContentItems = require('./getMultipleContentItems');
@@ -14,6 +14,7 @@ const getMultipleContentItems = require('./getMultipleContentItems');
 describe('getMultipleContentItems', function () {
   //extend timeout for mockgoose
   this.timeout(120000);
+  let testContent1, testContent2, testContent3, testContent4, testContent5;
   
   before(function (done) {
     this.timeout(120000);
@@ -27,22 +28,16 @@ describe('getMultipleContentItems', function () {
       .catch(err => done(err));
   });
   
-  afterEach(function (done) {
-    mockgoose.helper.reset().then(() => {
-      done();
-    });
-  });
-  
-  it('should all return all items if no query parameters are provided', function (done) {
-    const testContent1 = new ContentItem({
+  beforeEach(function (done) {
+    testContent1 = new ContentItem({
       author: 'Joseph Heller taco',
       content: '<p>taco</p>',
       description: 'taco',
       postType: 'blogPost',
       subtitle: 'a test subtitle taco',
-      title: 'Catch 22'
+      title: 'Catch 22 taco'
     });
-    const testContent2 = new ContentItem({
+    testContent2 = new ContentItem({
       author: 'Kurt Vonnegut',
       content: '<p>test content content taco</p>',
       description: 'taco',
@@ -50,7 +45,7 @@ describe('getMultipleContentItems', function () {
       subtitle: 'a test subtitle taco',
       title: 'Slaughterhouse 5'
     });
-    const testContent3 = new ContentItem({
+    testContent3 = new ContentItem({
       author: 'Mark Twain',
       content: '<p>test content content</p>',
       description: 'taco',
@@ -58,7 +53,7 @@ describe('getMultipleContentItems', function () {
       subtitle: 'a test subtitle taco',
       title: 'Huckleberry Finn'
     });
-    const testContent4 = new ContentItem({
+    testContent4 = new ContentItem({
       author: 'Ernest Hemingway',
       content: '<p>test content content</p>',
       description: 'a great book',
@@ -66,7 +61,7 @@ describe('getMultipleContentItems', function () {
       subtitle: 'a test subtitle taco',
       title: 'The Sun Also Rises'
     });
-    const testContent5 = new ContentItem({
+    testContent5 = new ContentItem({
       author: 'Henry James',
       content: '<p>test content content</p>',
       description: 'a horrible book',
@@ -76,131 +71,50 @@ describe('getMultipleContentItems', function () {
     });
     
     const promiseArray = [testContent1.save(), testContent2.save(), testContent3.save(), testContent4.save(), testContent5.save()];
-    const req = {};
     
     Promise.all(promiseArray)
-      .then(() => {
-        
-        getMultipleContentItems(req, {}, (err) => {
-          expect(err).not.to.exist;
-          expect(req.responseData.length).to.equal(5);
-          done();
-        });
-      })
+      .then(data => done())
       .catch(err => done(err));
+  });
+  
+  afterEach(function (done) {
+    mockgoose.helper.reset().then(() => {
+      done();
+    });
+  });
+  
+  it('should all return all items if no query parameters are provided', function (done) {
+    
+    const req = {};
+    
+    
+    getMultipleContentItems(req, {}, (err) => {
+      expect(err).not.to.exist;
+      expect(req.responseData.data.length).to.equal(5);
+      done();
+    });
     
   });
   
-  it('should all return all items sorted by relevance if req.query.q is provided, but req.query.limit is not provided', function (done) {
-    const testContent1 = new ContentItem({
-      author: 'Joseph Heller taco',
-      content: '<p>taco</p>',
-      description: 'taco',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle taco',
-      title: 'Catch 22 taco'
-    });
-    const testContent2 = new ContentItem({
-      author: 'Kurt Vonnegut',
-      content: '<p>test content content taco</p>',
-      description: 'taco',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle taco',
-      title: 'Slaughterhouse 5'
-    });
-    const testContent3 = new ContentItem({
-      author: 'Mark Twain',
-      content: '<p>test content content</p>',
-      description: 'taco',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle taco',
-      title: 'Huckleberry Finn'
-    });
-    const testContent4 = new ContentItem({
-      author: 'Ernest Hemingway',
-      content: '<p>test content content</p>',
-      description: 'a great book',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle taco',
-      title: 'The Sun Also Rises'
-    });
-    const testContent5 = new ContentItem({
-      author: 'Henry James',
-      content: '<p>test content content</p>',
-      description: 'a horrible book',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle',
-      title: 'The Portrait of a Lady'
-    });
-    
-    const promiseArray = [testContent3.save(), testContent2.save(), testContent5.save(), testContent4.save(), testContent1.save()];
+  it('should return all matching items sorted by relevance if req.query.q is provided, but req.query.limit is not provided', function (done) {
     const req = {
       query: {
         q: 'taco'
       }
     };
     
-    Promise.all(promiseArray)
-      .then(() => {
-        
-        getMultipleContentItems(req, {}, (err) => {
-          expect(err).not.to.exist;
-          expect(req.responseData.length).to.equal(5);
-          expect(req.responseData[0].attributes.title).to.equal(testContent1.title);
-          expect(req.responseData[1].attributes.title).to.equal(testContent2.title);
-          expect(req.responseData[2].attributes.title).to.equal(testContent3.title);
-          expect(req.responseData[3].attributes.title).to.equal(testContent4.title);
-          expect(req.responseData[4].attributes.title).to.equal(testContent5.title);
-          done();
-        });
-      })
-      .catch(err => done(err));
-    
+    getMultipleContentItems(req, {}, (err) => {
+      expect(err).not.to.exist;
+      expect(req.responseData.data.length).to.equal(4);
+      expect(req.responseData.data[0].attributes.title).to.equal(testContent1.title);
+      expect(req.responseData.data[1].attributes.title).to.equal(testContent2.title);
+      expect(req.responseData.data[2].attributes.title).to.equal(testContent3.title);
+      expect(req.responseData.data[3].attributes.title).to.equal(testContent4.title);
+      done();
+    });
   });
   
-  it('should the number of content items set in limit and sorted by relevance if req.query.limit and req.query.q are provided', function (done) {
-    const testContent1 = new ContentItem({
-      author: 'Joseph Heller taco',
-      content: '<p>taco</p>',
-      description: 'taco',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle taco',
-      title: 'Catch 22 taco'
-    });
-    const testContent2 = new ContentItem({
-      author: 'Kurt Vonnegut',
-      content: '<p>test content content taco</p>',
-      description: 'taco',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle taco',
-      title: 'Slaughterhouse 5'
-    });
-    const testContent3 = new ContentItem({
-      author: 'Mark Twain',
-      content: '<p>test content content</p>',
-      description: 'taco',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle taco',
-      title: 'Huckleberry Finn'
-    });
-    const testContent4 = new ContentItem({
-      author: 'Ernest Hemingway',
-      content: '<p>test content content</p>',
-      description: 'a great book',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle taco',
-      title: 'The Sun Also Rises'
-    });
-    const testContent5 = new ContentItem({
-      author: 'Henry James',
-      content: '<p>test content content</p>',
-      description: 'a horrible book',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle',
-      title: 'The Portrait of a Lady'
-    });
-    
-    const promiseArray = [testContent3.save(), testContent2.save(), testContent5.save(), testContent4.save(), testContent1.save()];
+  it('should return the number of content items set in limit, sorted by relevance, if req.query.limit and req.query.q are provided', function (done) {
     const req = {
       query: {
         q: 'taco',
@@ -208,137 +122,95 @@ describe('getMultipleContentItems', function () {
       }
     };
     
-    Promise.all(promiseArray)
-      .then(() => {
-        
-        getMultipleContentItems(req, {}, (err) => {
-          expect(err).not.to.exist;
-          expect(req.responseData.length).to.equal(2);
-          expect(req.responseData[0].attributes.title).to.equal(testContent1.title);
-          expect(req.responseData[1].attributes.title).to.equal(testContent2.title);
-          done();
-        });
-      })
-      .catch(err => done(err));
+    getMultipleContentItems(req, {}, (err) => {
+      expect(err).not.to.exist;
+      expect(req.responseData.data.length).to.equal(2);
+      expect(req.responseData.data[0].attributes.title).to.equal(testContent1.title);
+      expect(req.responseData.data[1].attributes.title).to.equal(testContent2.title);
+      done();
+    });
     
   });
   
-  it('should the number of content items set in limit unsorted if req.query.limit is provided, but req.query.q is not provided', function (done) {
-    const testContent1 = new ContentItem({
-      author: 'Joseph Heller taco',
-      content: '<p>taco</p>',
-      description: 'taco',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle taco',
-      title: 'Catch 22 taco'
-    });
-    const testContent2 = new ContentItem({
-      author: 'Kurt Vonnegut',
-      content: '<p>test content content taco</p>',
-      description: 'taco',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle taco',
-      title: 'Slaughterhouse 5'
-    });
-    const testContent3 = new ContentItem({
-      author: 'Mark Twain',
-      content: '<p>test content content</p>',
-      description: 'taco',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle taco',
-      title: 'Huckleberry Finn'
-    });
-    const testContent4 = new ContentItem({
-      author: 'Ernest Hemingway',
-      content: '<p>test content content</p>',
-      description: 'a great book',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle taco',
-      title: 'The Sun Also Rises'
-    });
-    const testContent5 = new ContentItem({
-      author: 'Henry James',
-      content: '<p>test content content</p>',
-      description: 'a horrible book',
-      postType: 'blogPost',
-      subtitle: 'a test subtitle',
-      title: 'The Portrait of a Lady'
-    });
-    
-    const promiseArray = [testContent3.save(), testContent2.save(), testContent5.save(), testContent4.save(), testContent1.save()];
+  it('should return the number of ContentItems set in the limit (unsorted) if req.query.limit is provided, but req.query.q is not provided', function (done) {
     const req = {
       query: {
         limit: '2'
       }
     };
     
-    Promise.all(promiseArray)
-      .then(() => {
-        
-        getMultipleContentItems(req, {}, (err) => {
-          expect(err).not.to.exist;
-          expect(req.responseData.length).to.equal(2);
-          expect(req.responseData[0].attributes).to.have.own.property('title');
-          expect(req.responseData[1].attributes).to.have.own.property('title');
-          expect(req.responseData[0].attributes).to.have.own.property('uniqueTitle');
-          expect(req.responseData[1].attributes).to.have.own.property('uniqueTitle');
-          done();
-        });
-      })
-      .catch(err => done(err));
-    
+    getMultipleContentItems(req, {}, (err) => {
+      expect(err).not.to.exist;
+      expect(req.responseData.data.length).to.equal(2);
+      expect(req.responseData.data[0].attributes).to.have.own.property('title');
+      expect(req.responseData.data[1].attributes).to.have.own.property('title');
+      expect(req.responseData.data[0].attributes).to.have.own.property('uniqueTitle');
+      expect(req.responseData.data[1].attributes).to.have.own.property('uniqueTitle');
+      done();
+    });
   });
   
-  it('should return an error object if no content items are found', function (done) {
-    const ContentItemStub = {
-      find: (query, callback) => {
-        callback(null, []);
-      }
-    };
+  it('should return an an empty array if no content items are found', function (done) {
     
-    // eslint-disable-next-line prefer-const
-    let getSingleContentItem = proxyquire('./getMultipleContentItems', {
-      '../models/ContentItem': ContentItemStub
-    });
     const req = {
       query: {
         q: 'pizza'
       }
     };
     
-    getSingleContentItem(req, {}, (err) => {
+    getMultipleContentItems(req, {}, (err) => {
       expect(err).not.to.exist;
       expect(req.responseData).to.deep.equal({
-        errors: [{
-          error: 'sorry we could not find anything',
-          status: 404
-        }]
+        data: [],
+        meta: {
+          totalItems: 0
+        },
+        status: 200
       });
       done();
     });
   });
   
-  it('should call the next with error if mongo returns an error', function (done) {
+  it('should return an empty array if there are no items in the collection', function (done) {
+    mockgoose.helper.reset()
+      .then(() => {
+        const req = {};
+        
+        getMultipleContentItems(req, {}, (err) => {
+          expect(err).to.not.exist;
+          expect(req.responseData).to.deep.equal({
+            data: [],
+            meta: {
+              totalItems: 0
+            }
+          });
+          done();
+        });
+        
+      });
+    
+  });
+  
+  it('should call next with error if mongo returns an error', function (done) {
     const testError = 'not enough cow bell';
     const nextSpy = sinon.spy();
-    const ContentItemStub = {
-      find: (query, callback) => {
-        callback(testError);
-      }
-    };
+    sinon.mock(ContentItem)
+      .expects('find')
+      //eslint-disable-next-line newline-per-chained-call
+      .chain('sort').withArgs({uniqueTitleKey: 'asc'})
+      .chain('exec')
+      .yields(testError);
     
-    // eslint-disable-next-line prefer-const
-    let getSingleContentItem = proxyquire('./getMultipleContentItems', {
-      '../models/ContentItem': ContentItemStub
-    });
     const req = {
       query: {
         q: 'pizza'
       }
     };
     
-    getSingleContentItem(req, {}, nextSpy);
+    getMultipleContentItems(req, {}, nextSpy);
     expect(nextSpy.calledWith(testError), 'next not correctly called with error').to.equal(true);
+    
+    // contentItemMock.resetBehvaior();
     done();
   });
   
